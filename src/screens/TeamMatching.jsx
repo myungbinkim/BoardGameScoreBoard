f328db593d1a2fc0da967281ea72a95743ec004c
myungbinkim <booksam11@naver.com>
2020-05-22 01:29:36 +0900

    team matching 수정

--- src/screens/TeamMatching.jsx
+++ src/screens/TeamMatching.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 const ArrayShuffle = (arr) => {
   let j;
@@ -15,51 +15,56 @@ const ArrayShuffle = (arr) => {
 };
 
 const FindNumberOfMembers = (arrsize) => {
-  let i;
-  for (i = 2; i <= arrsize; i += 1) {
-    if (arrsize % i === 0) break;
+  let num;
+  for (num = 2; num <= arrsize; num += 1) {
+    if (arrsize % num === 0) break;
   }
-  return i;
+
+  if (num === arrsize) num = 1;
+
+  return num;
 };
 
 function TeamTable(props) {
   const { data } = props;
+  const sortoptions = {
+    defaultSortName: 'rank',
+    defaultSortOrder: 'asc',
+  };
+
   return (
     <div>
-      <BootstrapTable data={data}>
-        <TableHeaderColumn isKey dataField="id">
-          No
-        </TableHeaderColumn>
-        <TableHeaderColumn dataField="member">
-          Member
-        </TableHeaderColumn>
-        <TableHeaderColumn dataField="rank">
-          expected rank
-        </TableHeaderColumn>
-        <TableHeaderColumn dataField="score">
-          expected score
-        </TableHeaderColumn>
+      <BootstrapTable data={data} options={sortoptions} striped>
+        <TableHeaderColumn dataField="rank" isKey dataSort dataAlign="center" width="50">예상 순위</TableHeaderColumn>
+        <TableHeaderColumn dataField="member" dataAlign="center" width="150">팀원</TableHeaderColumn>
       </BootstrapTable>
     </div>
   );
 }
 
 TeamTable.propTypes = {
-  data: PropTypes.arrayOf.isRequired,
+  data: PropTypes.arrayOf(PropTypes.shape({
+    rank: PropTypes.number.isRequired,
+    member: PropTypes.arrayOf(PropTypes.string).isRequired,
+  })).isRequired,
 };
 
 export default function TeamMatching() {
   // players는 인자로 받아야 함
-  const players = ['heejung', 'myungbin', 'jongchan', 'juhyun', 'woojin', 'jooho', 'sanghyun', 'sungwoo', 'soong', 'taeyun'];
+  const players = ['희정', '명빈', '종찬', '주현', '우진', '주호', '상현', '성우', '숭', '태윤'];
   ArrayShuffle(players);
-  const numberofmembers = FindNumberOfMembers(players.length);
-  const TeamCount = players.length / numberofmembers;
-  const Team = new Array(TeamCount);
-  for (let i = 0; i < TeamCount; i += 1) {
-    Team[i] = {
-      id: i, member: `${players[i * 2]} and ${players[i * 2 + 1]}`, rank: 1, score: 77,
-    };
+  const NumberOfMembers = FindNumberOfMembers(players.length);
+  const Team = [];
+  for (let i = 0; i < players.length; i += NumberOfMembers) {
+    Team.push({
+      rank: 1,
+      member: players.slice(i, i + NumberOfMembers),
+      score: 77 + i,
+    });
   }
+  Team.sort((a, b) => a.score - b.score);
+  Team.forEach((element, index) => { element.rank = index; });
+
   return (
     <TeamTable data={Team} />
   );
