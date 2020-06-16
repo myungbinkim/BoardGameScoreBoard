53b448f35a380878484e21549482e298f4c139d3
soong <soong@soongui-MacBookAir.local>
2020-06-16 22:18:13 +0900

    Add Result Table on GameOver Page

--- src/screens/GameOver.jsx
+++ src/screens/GameOver.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 /* bootstrap */
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 
 import { replaySelected } from '../redux/players';
@@ -60,23 +60,53 @@ const GameOver = () => {
 
   useEffect(() => postResult(teams, scores, players));
 
+  const worstTeam = teams.map((team) => ({
+    score: team.members.reduce((acc, m) => {
+      let total = acc;
+      scores.forEach((entry) => {
+        if (entry.id === m.id) {
+          total += entry.score;
+        }
+      });
+      return total;
+    }, 0),
+    ...team,
+  })).sort((a, b) => b.score - a.score);
+
   return (
     <Container>
-      <Row>
-        GAME OVER :)
-      </Row>
       <Link to="/score-board">
         <Button
+          variant="warning"
           onClick={() => {
             dispatch(replaySelected());
           }}
         >
           REPLAY
         </Button>
+        {' '}
       </Link>
       <Link to="/">
-        <Button>HOME</Button>
+        <Button variant="danger">HOME</Button>
       </Link>
+      <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>PLAYER</th>
+            <th>SCORE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {worstTeam.map((team, index) => (
+            <tr key={team.id}>
+              <td>{index + 1}</td>
+              <td>{team.members.map((m) => m.name).join(', ')}</td>
+              <td>{team.score}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </Container>
   );
 };
